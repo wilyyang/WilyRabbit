3e28be887341da073abf7e354ed142b58c122460
wilyyang <ehdrnr1178@gmail.com>
2021-01-05 16:26:33 +0900

    [WilyRabbit] Test AsyncTask Timer

--- app/src/main/java/wily/apps/wilyrabbit/Alarm.java
+++ app/src/main/java/wily/apps/wilyrabbit/Alarm.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 
 import androidx.core.app.NotificationCompat;
 
@@ -14,10 +15,55 @@ public class Alarm extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID).setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(CHANNEL_NAME).setContentText(CHANNEL_DESCRIPT+" [Alarm Ringing]").setAutoCancel(false);
+        TimerAsyncTask task = new TimerAsyncTask(context);
+        task.execute();
 
-        notificationManager.notify(4935, builder.build());
+    }
+
+    private class TimerAsyncTask extends AsyncTask<Void, Integer, Integer> {
+        private Context mContext;
+        private NotificationManager notificationManager;
+        private NotificationCompat.Builder builder;
+        public TimerAsyncTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+            builder = new NotificationCompat.Builder(mContext, CHANNEL_ID).setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(CHANNEL_NAME).setContentText("[Timer Start]").setAutoCancel(false)
+                    .setProgress(100,0, false);
+
+            notificationManager.notify(4935, builder.build());
+        }
+
+        @Override
+        protected Integer doInBackground(Void... voids) {
+            for(int count = 0; count<100; ++count){
+                publishProgress(count);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            builder.setProgress(100, values[0], false).setContentText("["+values[0]+"]");
+            notificationManager.notify(4935, builder.build());
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            builder.setProgress(0, 0, false).setContentText("[Timer End]");
+            notificationManager.notify(4935, builder.build());
+        }
     }
 }

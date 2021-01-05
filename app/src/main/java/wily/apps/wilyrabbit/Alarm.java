4f276c45dd24eef47300ea9988d52c28ac9dba0c
wilyyang <ehdrnr1178@gmail.com>
2021-01-05 11:50:14 +0900

    [WilyRabbit] Test alarm noti

--- app/src/main/java/wily/apps/wilyrabbit/Alarm.java
+++ app/src/main/java/wily/apps/wilyrabbit/Alarm.java
@@ -1,16 +1,23 @@
 package wily.apps.wilyrabbit;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
 
 public class Alarm extends BroadcastReceiver {
+    String CHANNEL_ID = "Wily Rabbit";
+    String CHANNEL_NAME = "Wily Rabbit";
+    String CHANNEL_DESCRIPT = "Wily Rabbit is good habbit";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Alarm", "Alarm Test ...");
-        Toast.makeText(context, "Alarm Test ...", Toast.LENGTH_SHORT).show();
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID).setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(CHANNEL_NAME).setContentText(CHANNEL_DESCRIPT+" [Alarm Ringing]").setAutoCancel(false);
+
+        notificationManager.notify(4935, builder.build());
     }
 }
--- app/src/main/java/wily/apps/wilyrabbit/MainActivity.java
+++ app/src/main/java/wily/apps/wilyrabbit/MainActivity.java
@@ -1,10 +1,16 @@
 package wily.apps.wilyrabbit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    String CHANNEL_ID = "Wily Rabbit";
+    String CHANNEL_NAME = "Wily Rabbit";
+    String CHANNEL_DESCRIPT = "Wily Rabbit is good habbit";
     public void registAlarm() {
         Intent intent = new Intent(this, Alarm.class);
         PendingIntent pIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
@@ -61,11 +70,24 @@ public class MainActivity extends AppCompatActivity {
         calendar.set(Calendar.MILLISECOND, 0);
 
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),  AlarmManager.INTERVAL_DAY, pIntent);
+
+        // Notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID).setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(CHANNEL_NAME).setContentText(CHANNEL_DESCRIPT+" ["+hour+":"+minute+"]").setAutoCancel(false);
+
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        notificationManager.notify(4935, builder.build());
     }
 
     public void unregistAlarm() {
         Intent intent = new Intent(this, Alarm.class);
         PendingIntent pIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
         alarmManager.cancel(pIntent);
+
+        // Notification
+        NotificationManagerCompat.from(this).cancel(4935);
     }
 }
